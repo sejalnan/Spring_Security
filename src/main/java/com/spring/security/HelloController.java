70e8aa42b9a416e496a2d52bf16e1623d16e0e24
sejalnandanwarab <sejalnandanwar302@gmail.com>
2026-02-20 22:06:08 +0530

    Updated Files

--- src/main/java/com/spring/security/HelloController.java
+++ src/main/java/com/spring/security/HelloController.java
@@ -1,12 +1,26 @@
 package com.spring.security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    // @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @PreAuthorize("hasRole('ADMIN')") //METHOD-LEVEL-SECURITY
     @GetMapping("/hello")
     public String hello(){
         return "Hello";
+    }
+
+    @GetMapping("/user/hello")
+    public String helloUser(){
+        return "Hello, USER";
+    }
+
+    @GetMapping("/admin/hello")
+    public String helloAdmin(){
+        return "Hello, ADMIN";
     }
 }
--- src/main/java/com/spring/security/SecurityConfig.java
+++ src/main/java/com/spring/security/SecurityConfig.java
@@ -3,21 +3,51 @@ package com.spring.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     //Enabling Basic Http authentication
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-        httpSecurity.authorizeHttpRequests(authorizeRequests->
-                authorizeRequests.anyRequest().authenticated());
+        httpSecurity.
+                authorizeHttpRequests(authorizeRequests->
+                        authorizeRequests.requestMatchers("/admin/**").hasRole("ADMIN")
+                                        .requestMatchers("/user/**").hasAnyRole("USER","ADMIN")
+                        .anyRequest().authenticated());
         httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
+
+    //In Memory Authentication
+    @Bean
+     public UserDetailsService userDetailsService(){
+        UserDetails user1= User.withUsername("user1")
+                .password("{noop}pass@01")
+                .roles("USER") //ROLE_USER
+                .build();
+
+         UserDetails admin= User.withUsername("admin") //ROLE_ADMIN
+                 .password("{noop}admin@01").roles("ADMIN")
+                 .build();
+
+         UserDetails user2= User.withUsername("user2")
+                 .password("{noop}pass@02")
+                 .roles("USER")
+                 .build();
+
+        return  new InMemoryUserDetailsManager(user1,user2,admin);
+     }
+
 }
