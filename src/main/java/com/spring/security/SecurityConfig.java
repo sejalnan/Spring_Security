9fd7d887f629977e91c41c0b7c5d5cf2cfb325bc
sejalnandanwarab <sejalnandanwar302@gmail.com>
2026-02-20 20:18:28 +0530

    Basic Authentication Configuration

---
+++ src/main/java/com/spring/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //Enabling Basic Http authentication
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.authorizeHttpRequests(authorizeRequests->
+                authorizeRequests.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
