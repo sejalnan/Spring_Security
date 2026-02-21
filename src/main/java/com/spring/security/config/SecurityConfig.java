acc9287b4f6239631e14219eccc688d526b09ca1
sejalnandanwarab <sejalnandanwar302@gmail.com>
2026-02-21 13:46:25 +0530

    Files Updated

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.spring.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,12 +32,13 @@ public class SecurityConfig {
     //Enabling Basic Http authentication
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-        httpSecurity.
-                authorizeHttpRequests(authorizeRequests->
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests->
                         authorizeRequests.requestMatchers("/admin/**").hasRole("ADMIN")
                                         .requestMatchers("/user/**").hasAnyRole("USER","ADMIN")
+                                .requestMatchers("/signin").permitAll()
                         .anyRequest().authenticated());
-        httpSecurity.httpBasic(Customizer.withDefaults());
+       // httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
@@ -62,9 +66,19 @@ public class SecurityConfig {
         // Connecting with database
         JdbcUserDetailsManager userDetailsManager=
                 new JdbcUserDetailsManager(datasource);
-        userDetailsManager.createUser(user1);
-        userDetailsManager.createUser(user2);
-        userDetailsManager.createUser(admin);
+
+        if (!userDetailsManager.userExists((user1.getUsername()))) {
+            userDetailsManager.createUser(user1);
+        }
+        if (!userDetailsManager.userExists((user2.getUsername()))) {
+            userDetailsManager.createUser(user2);
+        }
+        if (!userDetailsManager.userExists((admin.getUsername()))) {
+            userDetailsManager.createUser(admin);
+        }
+
+
+
 
         return userDetailsManager;
 
@@ -74,6 +88,11 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder){
+        return builder.getAuthenticationManager();
     }
 
 }
--- src/main/java/com/spring/security/controller/HelloController.java
+++ src/main/java/com/spring/security/controller/HelloController.java
@@ -1,11 +1,27 @@
 package com.spring.security.controller;
 
+import com.spring.security.JwtUtils;
+import com.spring.security.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    JwtUtils jwtUtils;
 
     // @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PreAuthorize("hasRole('ADMIN')") //METHOD-LEVEL-SECURITY
@@ -22,5 +38,30 @@ public class HelloController {
     @GetMapping("/admin/hello")
     public String helloAdmin(){
         return "Hello, ADMIN";
+    }
+
+    @PostMapping("/signin")
+    public String login(@RequestBody LoginRequest loginRequest){
+
+        Authentication authentication;
+        try{
+            authentication=authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),
+                            loginRequest.getPassword()
+                    )
+            );
+
+        }catch (AuthenticationException e){
+            e.printStackTrace();
+                    return "Could not authenticate";
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+       UserDetails userDetails= (UserDetails) authentication.getPrincipal();
+       String jwtToken=jwtUtils.generateTokenFromUsername(userDetails.getUsername());
+       return jwtToken;
+
     }
 }
